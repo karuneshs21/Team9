cc7290125f6b785f0aafc7aed4379975374af10c
Hyounjun Chang <hyounjunchang@g.ucla.edu>
2020-11-18 16:43:21 -0800

    Bug fix with listplayer/medialist not being initialized without playlist

--- modules/music_player/vlc_audio_player.py
+++ modules/music_player/vlc_audio_player.py
@@ -4,20 +4,25 @@ Related thread can be found on
 https://stackoverflow.com/questions/28440708/python-vlc-binding-playing-a-playlist
 """
 
-from vlc import Instance
+import vlc
 import time
 import os
 
 class VLC_Audio_Player:
     def __init__(self):
-        self.Player = Instance()
+        self.Player = vlc.Instance()
+        
+        self.listPlayer = self.Player.media_list_player_new()
+        self.listPlayer.set_playback_mode(vlc.PlaybackMode.loop) #loop if playlist over
+
+        self.mediaList = self.Player.media_list_new()
 
     def addPlaylist(self, paths):
         self.mediaList = self.Player.media_list_new()
         for path in paths:
             self.mediaList.add_media(self.Player.media_new(path))
         
-        self.listPlayer = self.Player.media_list_player_new()
+        #add the current playlist to list
         self.listPlayer.set_media_list(self.mediaList)
     def play(self):
         self.listPlayer.play()
