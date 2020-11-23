949e34d7a961ab9d98f56d38a8730bd9c758a8b8
Hyounjun Chang <hyounjunchang@g.ucla.edu>
2020-11-22 20:54:21 -0800

    Edited/Added More Buttons (Play/Pause/Random Playlist/Progress Bar)
    Added Helper functions to make these buttons possible

--- modules/music_player/vlc_audio_player.py
+++ modules/music_player/vlc_audio_player.py
@@ -18,12 +18,23 @@ class VLC_Audio_Player:
         self.mediaList = self.Player.media_list_new()
 
     def addPlaylist(self, paths):
+        """
+        Creates a playlist of files in the paths
+        Plays the first instance of playlist
+        """
+
+        self.listPlayer.stop() #stop the player to refresh
+        self.mediaList.release() #clear the old playlist
+
         self.mediaList = self.Player.media_list_new()
         for path in paths:
             self.mediaList.add_media(self.Player.media_new(path))
         
-        #add the current playlist to list
+        #replace to current playlist
         self.listPlayer.set_media_list(self.mediaList)
+
+        self.listPlayer.play()
+
     def play(self):
         self.listPlayer.play()
     def next(self):
@@ -33,4 +44,30 @@ class VLC_Audio_Player:
     def previous(self):
         self.listPlayer.previous()
     def stop(self):
-        self.listPlayer.stop()
+        self.listPlayer.get_media_player().stop()
+
+    def is_playing(self):
+        """
+        returns true if media is playing, false otherwise.
+        """
+        return bool(self.listPlayer.is_playing())
+
+    def get_time(self):
+        """
+        returns time in ms in current media
+        """
+        return self.listPlayer.get_media_player().get_time()
+
+    def set_time(self, ms):
+        self.listPlayer.get_media_player().set_time(ms)
+
+    def get_length(self):
+        return self.listPlayer.get_media_player().get_length()
+    
+    def clear_playlist(self):
+        """
+        Empties vlc mediaList (object for playlist)
+        Songs will still be played through playlist
+        """
+        self.mediaList.release()
+        self.mediaList = self.Player.media_list_new()
